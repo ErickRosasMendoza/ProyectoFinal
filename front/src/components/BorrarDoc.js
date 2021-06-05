14e360062e9263eb4757c57e96feeced39ed353d
Gervac1o <megatroms@gmail.com>
2021-06-04 21:57:07 -0500

    no message

--- front/src/components/BorrarDoc.js
+++ front/src/components/BorrarDoc.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-import DirectorioAdmin from './DirectorioAdmin';
-import Slider from './Slider';
 
 
 class BorrarDoc extends React.Component {
